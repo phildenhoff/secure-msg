801580e5e923e44f55cd6c91b3b508944fc39aaa
phildenhoff <phildenhoff@gmail.com>
2017-11-09 00:09:20 -0800

    Add initConnection function
    Added interface function initConnection which will initialize a client-server connection.

--- Message.java
+++ Message.java
@@ -1,6 +1,8 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Message extends Remote {
-    void receiveMessage(String msg) throws RemoteException;
+public interface Message extends Remote{
+    void receiveMessage (String msg) throws RemoteException;
+    void receivePackage (MessagePackage pkg) throws RemoteException;
+    boolean initConnection (MessagePackage pkg) throws RemoteException;
 }

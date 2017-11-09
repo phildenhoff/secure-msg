3c42ca7e34ea9ffac257166f25dc39f624c1f77d
Jackie Gorman <jackiegorman.4@hotmail.com>
2017-11-09 13:50:54 -0800

    Added documentation to methods

--- MessagePackage.java
+++ MessagePackage.java
@@ -45,7 +45,7 @@ class MessagePackage implements java.io.Serializable {
     }
 
     /**
-     * Returns the fingeprint from a MessagePackage
+     * Returns the fingerprint from a MessagePackage
      * 
      * @return fingerprint as a String
      */
@@ -53,38 +53,79 @@ class MessagePackage implements java.io.Serializable {
         return this.msg;
     }
 
+	/**
+     * Sets initialization options for confidentiality, integrity, and authentication
+     * 
+     * @param boolean conf: True if confidentiality selected
+     * @param boolean integ: True if integrity selected
+	 * @param boolean auth: True if authentication selected
+     */
     public void setInitOptions (boolean conf, boolean integ, boolean auth) {
         options = new boolean[] {conf, integ, auth};
     }
 
+	/**
+     * Returns the boolean array containing selected options for confidentiality, integrity, and authentication
+     * 
+     * @return options as a boolean array
+     */
     public boolean[] getOptions () {
         return options;
     }
 
+	/**
+     * Returns the initialization vector used for encryption
+     * 
+     * @return iv as a byte array
+     */
     public byte[] getIV () {
         return iv;
     }
 
+	/**
+     * Sets the symmetric key
+     */
     public void setSymmSecretKey (SecretKey symmetricKey) {
         this.symmetricKey = symmetricKey;
     }
 
+	/**
+     * Returns the symmetric key
+     * 
+     * @return the symmetric key used as a SecretKey
+     */
     public SecretKey getSymmSecretKey () {
        return this.symmetricKey;
     }
 
+	/**
+     * Sets the public key
+     */
     public void setPublicKey (PublicKey pubKey) {
         this.pubKey = pubKey;
     }
 
+	/**
+     * Returns the fingerprint from a MessagePackage
+     * 
+     * @return pubKey as a public key
+     */
     public PublicKey getPublicKey () {
        return this.pubKey;
     }
 
+	/**
+     * Sets the device name
+     */
     public void setDeviceName (String deviceName) {
         this.deviceName = deviceName;
     }
 
+	/**
+     * Returns the device name
+     * 
+     * @return deviceName as String
+     */
     public String getDeviceName () {
         return this.deviceName;
     }
